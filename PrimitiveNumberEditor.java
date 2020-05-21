abae0cc8e3d702df65285c0c07e5b8d711a72d88
david7838 <65706909+david7838@users.noreply.github.com>
2020-05-21 20:16:02 +0800

    Update PrimitiveNumberEditor.java

--- PrimitiveNumberEditor.java
+++ PrimitiveNumberEditor.java
@@ -1,10 +1,4 @@
 
 	private final boolean allowEmpty;
-	public PrimitiveNumberEditor(Class<? extends Number> numberClass, boolean allowEmpty) throws IllegalArgumentException {
-		this(numberClass, null, allowEmpty);
-	}
-	public PrimitiveNumberEditor(Class<? extends Number> numberClass, NumberFormat numberFormat, boolean allowEmpty) throws IllegalArgumentException {
-		super(numberClass, numberFormat, allowEmpty);
-		this.allowEmpty = allowEmpty;
-	}
+
 
