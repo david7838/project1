2d08f8d8e30dd99dc384aa488669e29814fce04a
david7838 <forbid7838@gmail.com>
2020-05-21 20:25:30 +0800

    回覆

--- PrimitiveNumberEditor.java
+++ PrimitiveNumberEditor.java
@@ -1,9 +1,21 @@
 
 	private final boolean allowEmpty;
+	public PrimitiveNumberEditor(Class<? extends Number> numberClass, boolean allowEmpty) throws IllegalArgumentException {
+		this(numberClass, null, allowEmpty);
+	}
+	public PrimitiveNumberEditor(Class<? extends Number> numberClass, NumberFormat numberFormat, boolean allowEmpty) throws IllegalArgumentException {
+		super(numberClass, numberFormat, allowEmpty);
+		this.allowEmpty = allowEmpty;
+	}
+	@Override
+	public void setAsText(String text) throws IllegalArgumentException {
+		if(allowEmpty && (text==null || text.length()==0)) {
+			super.setAsText("0");
+		} else {
+			super.setAsText(text);
+		}
+	}
+}
 
-<<<<<<< HEAD
 11111111111111111111111111
 ssss
-=======
-
->>>>>>> abae0cc8e3d702df65285c0c07e5b8d711a72d88
